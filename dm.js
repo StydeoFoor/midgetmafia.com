d8ec81f2aae99b868f18f754e81107a64bbb5032
StydeoFoor <stydeofoor@gmail.com>
2024-12-20 15:04:29 -0600

    after doing it for 5 hours

--- dm.js
+++ dm.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
-  import { getDatabase, ref, set, get, onValue } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+  import { getDatabase, ref, set, get, onValue } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
 // Firebase Initialization
 const firebaseConfig = {
   apiKey: "AIzaSyB17qMT--ON4KaYZLnEjU5HbwZmds9KgWg",
