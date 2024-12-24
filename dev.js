03731194419134d5cd53d902b15c0734ce4fec4d
StydeoFoor <stydeofoor@gmail.com>
2024-12-23 19:01:37 -0600

    WHAT THY SIGMAAAAAA

--- dev.js
+++ dev.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
+const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
 
 function checkUser() {
     if (!loggedInUser) {
